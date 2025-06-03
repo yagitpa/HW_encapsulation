b022d8d021dcbaa16155ca65ac0c03c23e1badb7
yagitpa <yakov.palchikov@gmail.com>
2025-06-04 00:58:10 +0300

    Homework 2 done!

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,43 +2,33 @@ package org.skypro.skyshop.product;
 
 import java.util.Objects;
 
-public final class Product {
-    private final String name;
-    private final int price;
+public abstract class Product {
+    protected final String name;
 
-    public Product(String name, int price) {
+    public Product(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Наименование товара не может быть пустым или отсутствовать!");
         }
-        if (price < 0) {
-            throw new IllegalArgumentException("Цена товара не может быть отрицательной!");
-        }
         this.name = name;
-        this.price = price;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPrice() {
-        return price;
-    }
+    public abstract double getPrice();
+
+    public abstract boolean isSpecial();
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return price == product.price && Objects.equals(name, product.name);
+        return Objects.equals(name, product.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, price);
-    }
-
-    @Override
-    public String toString() {
-        return "Товар: " + name + ", Цена: " + price + "р.";
+        return Objects.hashCode(name);
     }
 }
