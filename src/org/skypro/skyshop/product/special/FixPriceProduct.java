bdf875eeca8b3460111e4d05da9383843cce9051
yagitpa <yakov.palchikov@gmail.com>
2025-08-17 17:42:59 +0300

    HW Set done! + fix comparator class + fix equals() and hashCode() for successor classes

--- src/org/skypro/skyshop/product/special/FixPriceProduct.java
+++ src/org/skypro/skyshop/product/special/FixPriceProduct.java
@@ -26,22 +26,5 @@ public class FixPriceProduct extends Product {
     public boolean isSpecial() {
         return true;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o != null || getClass() != o.getClass()) {
-            return false;
-        }
-        FixPriceProduct that = (FixPriceProduct) o;
-        return super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode());
-    }
 }
 
